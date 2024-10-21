ea00d929c511bda98c573b507de07d12c7455518
David Schoots <530868@student.glu.nl>
2024-10-21 09:53:20 +0200

    added button to main menu | backup 21-10-2024

--- routes/web.php
+++ routes/web.php
@@ -5,31 +5,6 @@ use Statamic\Facades\Entry;
 use Statamic\Facades\Content;
 
 
-
-
-// Route::statamic('example', 'example-view', [
-//    'title' => 'Example'
-// ]);
-
-/*  Route::statamic('article/{slug}', 'detail', [
-    'title' => 'Example'
-]); */
-
-/* Route::statamic('articles/{slug}', 'detail');
- *//* Route::get('/articles/{slug}', function ($slug) {
-
-    $articles = Statamic::api('collections')->findBySlug($slug, 'articles');
-
-    if (!$articles){
-        abort(404);
-    }
-
-    return view('articles.show',['aricles' => $articles]);
- }); */
-
-
-
-
 Route::get('articles/{slug}', function ($slug) {
     // Zoekt een artikel in de 'articles' collectie op basis van de slug
     $search = Entry::query()->where('slug', $slug)->where('collection', 'articles')->first();
@@ -41,6 +16,8 @@ Route::get('articles/{slug}', function ($slug) {
 
     // Geeft de detail view weer en stuurt de gevonden content mee
     /* de content is de word meegestuurd naar de pagina */
-    return view('detail', ['content' => $search]);
+    // return view('detail', ['content' => $search]);
+    return (new \Statamic\View\View)->template('detail')->layout('layout')->with(['content' => $search]);
 });
+
 
